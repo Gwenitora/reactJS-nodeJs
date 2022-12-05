76e127a35580d582777b2b3699ed087fbd72dec4
Gwenitora <gwenitora.gardu74@gmail.com>
2022-12-05 14:18:29 +0100

    init

---
+++ db/db.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+const connectionString =
+  "mongodb+srv://root:RHvon6j9SKi4wH75@pokedex.mlhfr4h.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return err;
+      }
+      
+      //remplacer whatever par le nom de votre DB !
+      dbConnection = db.db("whatever");
+      console.log("Successfully connected to MongoDB.");
+    });
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const dbo = require("./db/db");
 const app = express();
 const port = 4444;
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
+dbo.connectToServer();
 
-app.listen(port, function () {
-  console.log(`App listening on port ${port}!`);
-});
+/*
+suite du code ici
+*/
