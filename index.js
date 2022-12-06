e26b7e298c896384e8daf380127b65c23534fc24
Gwenitora <gwenitora.gardu74@gmail.com>
2022-12-06 13:14:47 +0100

    feature: mangoDB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dbo = require("./db/db");
 const app = express();
 const port = 4444;
 const bodyParser = require('body-parser');
+const { ObjectId } = require("mongodb");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const jsonParser = bodyParser.json();
@@ -11,7 +12,7 @@ dbo.connectToServer();
 app.get("/pokemon/list", function (req, res) {
   const dbConnect = dbo.getDb();
   const collPokemon = dbConnect.collection("pokemons");
-  collPokemon.find({})
+  collPokemon.find()
     .toArray(function (err, result) {
       if (err) {
         res.status(400).send("Error fetching pokemons!");
@@ -24,11 +25,31 @@ app.get("/pokemon/list", function (req, res) {
 
 app.post('/pokemon/insert', jsonParser, (req, res) => {
   const body = req.body;
-  console.log('Got body:', body);
   res.json(body);
+  console.log('Insert:', body);
   const dbConnect = dbo.getDb();
   const collPokemon = dbConnect.collection("pokemons");
   collPokemon.insert(body);
+});
+
+app.delete('/pokemon/delete', jsonParser, (req, res) => {
+  const body = req.body;
+  res.json(body);
+  console.log('Delete:', body);
+  const dbConnect = dbo.getDb();
+  const collPokemon = dbConnect.collection("pokemons");
+  collPokemon.deleteOne({
+    _id: ObjectId(body._id)
+  });
+});
+
+app.post('/pokemon/update', jsonParser, (req, res) => {
+  const body = req.body;
+  res.json(body);
+  console.log('Update:', body);
+  const dbConnect = dbo.getDb();
+  const collPokemon = dbConnect.collection("pokemons");
+  collPokemon.updateOne(body.before, {$set: body.after});
 });
 
 app.listen(port, function () {
