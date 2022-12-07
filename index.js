b43a6ee761fda77014300514dd3cbbda259e3c23
Gwenitora <gwenitora.gardu74@gmail.com>
2022-12-07 15:26:18 +0100

    index

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const jsonParser = bodyParser.json();
 dbo.connectToServer();
 
-const body = function(req, res){
+const body = function(req){
   const body = req.body;
+  if (body._id) {
+    body._id = ObjectId(body._id);
+  }
   return body;
 }
 const listBDD = function(collection, body, res){
@@ -37,51 +40,51 @@ const deleteBDD = function(collection, body, res){
   console.log('Delete ', collection, ':', body);
   const dbConnect = dbo.getDb();
   const collPokemon = dbConnect.collection(collection);
-  collPokemon.deleteOne({_id: ObjectId(body._id)});
+  collPokemon.deleteOne(body);
 };
 const updateBDD = function(collection, body, res){
   res.json(body);
   console.log('Update ', collection, ':', body);
   const dbConnect = dbo.getDb();
   const collPokemon = dbConnect.collection(collection);
-  collPokemon.updateOne({_id: ObjectId(body.before._id)}, {$set: body.after});
+  collPokemon.updateOne(body.before, {$set: body.after});
 };
 
 // Pokémons list
 app.post("/type/list",jsonParser, function (req, res) {
-  listBDD("types", body(req, res), res);
+  listBDD("types", body(req), res);
 });
 app.post('/type/insert', jsonParser, (req, res) => {
-  insertBDD("types", body(req, res), res);
+  insertBDD("types", body(req), res);
 });
 app.delete('/type/delete', jsonParser, (req, res) => {
-  deleteBDD("types", body(req, res), res);
+  deleteBDD("types", body(req), res);
 });
 app.post('/type/update', jsonParser, (req, res) => {
-  updateBDD("types", body(req, res), res);
+  updateBDD("types", body(req), res);
 });
 // Pokémons list
 app.post("/pokemon/list",jsonParser, function (req, res) {
-  listBDD("pokemons", body(req, res), res);
+  listBDD("pokemons", body(req), res);
 });
 app.post('/pokemon/insert', jsonParser, (req, res) => {
-  insertBDD("pokemons", body(req, res), res);
+  insertBDD("pokemons", body(req), res);
 });
 app.delete('/pokemon/delete', jsonParser, (req, res) => {
-  deleteBDD("pokemons", body(req, res), res);
+  deleteBDD("pokemons", body(req), res);
 });
 app.post('/pokemon/update', jsonParser, (req, res) => {
-  updateBDD("pokemons", body(req, res), res);
+  updateBDD("pokemons", body(req), res);
 });
 // Pokadex
 app.post("/pokadex/list",jsonParser, function (req, res) {
-  listBDD("pokadex", body(req, res), res);
+  listBDD("pokadex", body(req), res);
 });
 app.post('/pokadex/insert', jsonParser, (req, res) => {
   insertBDD("pokadex", listBDD("pokemons", body(req, res), res), res);
 });
 app.delete('/pokadex/delete', jsonParser, (req, res) => {
-  deleteBDD("pokadex", body(req, res), res);
+  deleteBDD("pokadex", body(req), res);
 });
 
 app.listen(port, function () {
