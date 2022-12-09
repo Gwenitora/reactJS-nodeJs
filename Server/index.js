fcc2c311588872de30f874c14554d98e07442b8b
Gwenitora <gwenitora.gardu74@gmail.com>
2022-12-09 15:25:38 +0100

    debug: api

--- Server/index.js
+++ Server/index.js
@@ -9,12 +9,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const jsonParser = bodyParser.json();
 dbo.connectToServer();
 
-const body = function(req){
-  let body = req.body;
+const body = async function(req){
+  let body = await req.body;
   if (body._id) {
-    body._id = ObjectId(body._id);
+    body._id = await ObjectId(body._id);
   }
-  return body;
+  return await new Promise(res => {res(body)});
 }
 const listBDD = async (collection, body) => {
   return await new Promise(res => {
@@ -46,54 +46,79 @@ const getID = function(array){
   array.forEach(element => 
     ids.push(element._id)
   );
+  return ids;
 };
 
 // Types
 app.post("/type/list",jsonParser, async function (req, res) {
-  const result = await listBDD("types", body(req, res), res);
+  const bod = await body(req, res);
+  const result = await listBDD("types", bod);
   res.json(result);
 });
-app.post('/type/insert', jsonParser, (req, res) => {
-  insertBDD("types", body(req, res));
+app.post('/type/insert', jsonParser, async (req, res) => {
+  const bod = await body(req, res);
+  insertBDD("types", bod);
 });
-app.delete('/type/delete', jsonParser, (req, res) => {
-  deleteBDD("types", body(req, res));
+app.delete('/type/delete', jsonParser, async (req, res) => {
+  const bod = await body(req, res);
+  deleteBDD("types", bod);
 });
-app.post('/type/update', jsonParser, (req, res) => {
-  updateBDD("types", body(req, res));
+app.post('/type/update', jsonParser, async (req, res) => {
+  const bod = await body(req, res);
+  updateBDD("types", bod);
 });
 // Pokémons 
 app.post("/pokemon/list",jsonParser, async (req, res) => {
-  const result = await listBDD("pokemons", body(req, res), res).then((hh)=>hh);
+  const bod = await body(req, res);
+  const result = await listBDD("pokemons", bod);
   await res.json(result);
 });
 app.post('/pokemon/insert', jsonParser, async (req, res) => {
-  insertBDD("pokemons", body(req, res));
-  var type = await listBDD("types", body(req, res));
-  type = getID(type)
-  var insertType = [body(req, res), type];
-  updateBDD("pokemons", insertType);
+  const bod = await body(req, res);
+  insertBDD("pokemons", bod);
+  if (bod.type) {
+    var type = await listBDD("types", bod.type);
+    type = await getID(type)
+    var insertType = {before: bod.name, after: {type: type}};
+    updateBDD("pokemons", insertType);
+  }
 });
-app.delete('/pokemon/delete', jsonParser, (req, res) => {
-  deleteBDD("pokemons", body(req, res));
+app.delete('/pokemon/delete', jsonParser, async (req, res) => {
+  const bod = await body(req, res);
+  deleteBDD("pokemons", bod);
 });
-app.post('/pokemon/update', jsonParser, (req, res) => {
-  updateBDD("pokemons", body(req, res));
+app.post('/pokemon/update', jsonParser, async (req, res) => {
+  const bod = await body(req, res);
+  updateBDD("pokemons", bod);
+  if (bod.after.type) {
+    var type = await listBDD("types", bod.after.type);
+    type = await getID(type)
+    var insertType = {before: bod.before, after: {type: type}};
+    updateBDD("pokemons", insertType);
+  }
 });
 // Pokadex
 app.post("/pokadex/list",jsonParser, async function (req, res) {
-  const result = await listBDD("pokadex", body(req, res));
+  const bod = await body(req, res);
+  const result = await listBDD("pokadex", bod);
   res.json(result)
 });
 app.post('/pokadex/insert', jsonParser, async (req, res) => {
-  var pokemon = await listBDD("pokemons", body(req, res));
-  pokemon = getID(pokemon)
-  insertBDD("pokadex", pokemon);
+  const bod = await body(req, res);
+  var pokemons = await listBDD("pokemons", bod);
+  pokemons = getID(pokemons)
+  console.log('multi', pokemons);
+  pokemons.forEach(pokemon => {
+    console.log('solo', pokemon);
+    insertBDD("pokadex", {pokaID: pokemon})
+  });
 });
-app.delete('/pokadex/delete', jsonParser, (req, res) => {
-  deleteBDD("pokadex", body(req, res));
+app.delete('/pokadex/delete', jsonParser, async (req, res) => {
+  const bod = await body(req, res);
+  deleteBDD("pokadex", bod);
 });
 
+// Listen
 app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
 });
