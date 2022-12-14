101c31f7598a47ff4cfdb0c42a6c0937b38881be
Gwenitora <gwenitora.gardu74@gmail.com>
2022-12-14 19:03:16 +0100

    feature: RESPONSIVE

--- Client/myapp/src/pages/home.js
+++ Client/myapp/src/pages/home.js
@@ -4,18 +4,26 @@ export function Home(){
     {document.title = "Home - Pokadex"}
    
     return (<>
+    <div className="title-home">
+        <img src="/img/welcome2.png" className="carapuce" />
+        <h1>BIENVENUE SUR POKAPEDIA</h1>
+        <img src="/img/welcome2.png" className="carapuce reverse" />
+    </div>
     <div className="home">
-        <img src="/img/welcome.png" className="hey" />
-        <h1 className="title" >BIENVENUE SUR POKAPEDIA</h1>
-        <img src="/img/welcome2.png" className="hey2" />
-        <img src="/img/welcome2.png" className="hey3" />
-        <img src="/img/welcome4.png" className="hey4" />
-        <form>
+        <div className="pikachu">
+            <img src="/img/welcome.png"/>
+        </div>
+        {/* <form>
             <button className="admin-btn" type="button">Admin</button>
-        </form>
-        <div className="paragraph">
-            <p>Bienvenue sur pokapedia votre pokedex de reférence en ligne. Sur pokapedia vous pourrez trouver tout vos pokemons favoris mais aussi en connaitres d'autres. Pokapedia est un site libre d'acces. Les developpeurs de ce site se nommes Kyllian et Gwendal. Deux etudiants à Gaming Campus. </p>
-            <p>Les devoloppeurs vous souhaitent une bonne experience sur pokepedia et vous souhaite également à toutes et à tous de devenir les meilleurs dresseurs/dresseuses</p>
+        </form> */}
+        <div className="paragraph-home">
+            <div className="img">
+                <img src="/img/welcome4.png" className="sasha-pika-home" />
+            </div>
+            <div className="text">
+                <p>Bienvenue sur pokapedia votre pokedex de reférence en ligne. Sur pokapedia vous pourrez trouver tout vos pokemons favoris mais aussi en connaitres d'autres. Pokapedia est un site libre d'acces. Les developpeurs de ce site se nommes Kyllian et Gwendal. Deux etudiants à Gaming Campus.
+                <br />Les devoloppeurs vous souhaitent une bonne experience sur pokepedia et vous souhaite également à toutes et à tous de devenir les meilleurs dresseurs/dresseuses</p>
+            </div>
         </div>
     </div>
     </>);
